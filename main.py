3dc53c9a38dfd1bd313805b74541a7e9c99ce6eb
xsonykkx <sofiya.strologo@gmail.com>
2024-07-05 13:38:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 or 1000 !=1000.1)
+#3rd program
+a = 1234
+b = 5678
+middle1 = a % 1000 // 10
+middle2 = b % 1000 // 10
+print (middle1 + middle2)
+# 4th program
+a = 13.42
+b =  42.13
+b1 = int(b)
+c = 13.42
+c1 = int(c)
+d = 42.13
+print (c1 == d * 100 % 100 or b1 == d * 100 % 100)
